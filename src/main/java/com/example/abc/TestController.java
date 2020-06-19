674a5605007fea6baefd9f31f205d7996ba4b90c
szn1995 <sujan.koju.sastra@gmail.com>
2020-06-19 09:01:12 +0545

    zxczxc

--- src/main/java/com/example/abc/TestController.java
+++ src/main/java/com/example/abc/TestController.java
@@ -8,7 +8,7 @@ public class TestController {
     @GetMapping("test")
     public String test() {
         System.out.println("HERE I AM SUJAN");
-        return "HELLO WORLD MY NAME IS SUJAN Koju  HERO OF NEPALaaa";
+        return "HELLO ";
     }
 
 }
